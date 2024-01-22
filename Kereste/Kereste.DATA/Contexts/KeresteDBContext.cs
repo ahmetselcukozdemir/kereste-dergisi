445941351bea5ab1f94d6447998122da7ba33066
Ahmet Selçuk Özdemir <ahmetselcukozdemir01@gmail.com>
2024-01-23 01:04:56 +0300

    23-01-2024-v2

--- Kereste/Kereste.DATA/Contexts/KeresteDBContext.cs
+++ Kereste/Kereste.DATA/Contexts/KeresteDBContext.cs
@@ -24,6 +24,6 @@ namespace Kereste.DATA.Contexts
 
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-	   => optionsBuilder.UseSqlServer("Server=213.238.182.20;Initial Catalog=KeresteDergiDB;Persist Security Info=False;User ID=asobey;Password=aHMET.1997!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;");
+	   => optionsBuilder.UseSqlServer("Server=**************;Initial Catalog=KeresteDergiDB;Persist Security Info=False;User ID=************;Password=**********;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;");
 	}
 }
